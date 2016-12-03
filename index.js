18da83693dc0e97822b6de3c47b652c2d48adbe0
Julianna Stevenson <steve146@purdue.edu>
2016-12-02 20:32:45 -0500

    connected to database

--- index.js
+++ index.js
@@ -17,8 +17,25 @@ app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 });
 
-client.sendMessage({
-  to: '5743399220',
-  from: '5743672678',
-  body: 'Hi Julie! This is your new app!'
+//client.sendMessage({
+//  to: '5743399220',
+//  from: '5743672678',
+//  body: 'Hi Julie! This is your new app!'
+//});
+
+MongoClient.connect(DataBaseURL, function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+    var collection = db.collection('test');
+    var dataToInsert = {hello: 'world'};
+
+    collection.insert(dataToInsert, function(err, doc) {
+      collection.findOne({hello: 'world'}, function(err, item) {
+        console.log('found it!', item);
+      });
+    });
+  }
+  else {
+    console.log("ERROR: Couldn't connect to database.", err)
+  }
 });
